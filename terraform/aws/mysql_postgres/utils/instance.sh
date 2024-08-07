0cd8d898bb5416d8238e7044c9389baa9d87eb95
CM <cmutzlitz@confluent.io>
2024-08-07 17:42:02 +0200

    Update instance.sh

--- terraform/aws/mysql_postgres/utils/instance.sh
+++ terraform/aws/mysql_postgres/utils/instance.sh
@@ -23,25 +23,19 @@ curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compos
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# install KSQLDB Demo
+# install CDC Workshop Setup
 cd /home/ec2-user
-wget ${confluent_ksqldb_demo}
-unzip master.zip
-chown ec2-user:ec2-user -R /home/ec2-user/confluent-ksqldb-hands-on-workshop-master/
-rm master.zip
-chown ec2-user:ec2-user -R confluent-ksqldb-hands-on-workshop-master/*
-cd /home/ec2-user/confluent-ksqldb-hands-on-workshop-master/
-rm -rf terraform/
+wget ${confluent_cdc_workshop}
+unzip main.zip
+chown ec2-user:ec2-user -R /home/ec2-user/confluent-cdc-workshop-main/
+rm main.zip
+chown ec2-user:ec2-user -R confluent-cdc-workshop-main/*
+cd /home/ec2-user/confluent-cdc-workshop-main/terraform/aws/mysql_postgres
+mv docker /home/ec2-user/
+cd /home/ec2-user/
+rm -rf confluent-cdc-workshop-main/
 
-
-# set PUBLIC IP and change the Data in docker-compose.yaml
-cd /home/ec2-user/confluent-ksqldb-hands-on-workshop-master/docker/
-PUBIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-SCRIPT1="sed -i 's/CONNECT_REST_ADVERTISED_HOST_NAME: connect-ext/CONNECT_REST_ADVERTISED_HOST_NAME: $PUBIP/g' docker-compose.yml;"
-SCRIPT2="sed -i 's/CONTROL_CENTER_KSQL_WORKSHOP_ADVERTISED_URL: http:\/\/localhost:8088/CONTROL_CENTER_KSQL_WORKSHOP_ADVERTISED_URL: http:\/\/$PUBIP:8088/g' docker-compose.yml;"
-#SCRIPT4="sed -i 's/KAFKA_ADVERTISED_LISTENERS: PLAINTEXT:\/\/kafka:29092,PLAINTEXT_HOST:\/\/localhost:9092/KAFKA_ADVERTISED_LISTENERS: PLAINTEXT:\/\/kafka:29092,PLAINTEXT_HOST:\/\/$PUBIP:9092/g' docker-compose.yml;"
-# change docker-compose file with public IP for advertised properties 
-bash -c "$SCRIPT1"
-bash -c "$SCRIPT2"
-#bash -c "$SCRIPT4"
+# run docker compose
+cd docker
+docker-compose up -d
 
