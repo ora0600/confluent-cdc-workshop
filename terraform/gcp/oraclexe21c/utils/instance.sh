cfd2aa9714dfedc7e3efd9b3f53d734431be8fba
CM <cmutzlitz@confluent.io>
2024-10-02 14:11:38 +0200

    Update instance.sh for GCP

--- terraform/gcp/oraclexe21c/utils/instance.sh
+++ terraform/gcp/oraclexe21c/utils/instance.sh
@@ -17,7 +17,11 @@ yum install jq -y
 yum install expect -y
 yum install nc -y
 # install docker
-yum install -y docker
+#yum install -y docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin --allowerasing -y
+systemctl start docker
 # install java 11
 yum install java-11-openjdk -y
 # set environment
@@ -25,12 +29,6 @@ echo vm.max_map_count=262144 >> /etc/sysctl.conf
 sysctl -w vm.max_map_count=262144
 echo "    *       soft  nofile  65535
     *       hard  nofile  65535" >> /etc/security/limits.conf
-sed -i -e 's/1024:4096/65536:65536/g' /etc/sysconfig/docker
-# enable docker    
-usermod -a -G docker ec2-user
-service docker start
-chkconfig docker on
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
@@ -42,7 +40,7 @@ cd /home/${compute_user}
 wget ${cdc_url}
 unzip main.zip
 chown ${compute_user}:${compute_user} -R /home/${compute_user}/confluent-cdc-workshop-main/
-rm main.zip
+rm --force main.zip
 chown ${compute_user}:${compute_user} -R confluent-cdc-workshop-main/*
 cd /home/${compute_user}/confluent-cdc-workshop-main/terraform/gcp/oraclexe21c
 mv docker/ /home/${compute_user}/
