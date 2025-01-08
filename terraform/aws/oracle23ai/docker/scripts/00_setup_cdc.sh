3f2ec2f5d1321167218848c2daec6dea1b47b2da
CM <cmutzlitz@confluent.io>
2025-01-08 18:11:22 +0100

    Update 00_setup_cdc.sh

--- terraform/aws/oracle23ai/docker/scripts/00_setup_cdc.sh
+++ terraform/aws/oracle23ai/docker/scripts/00_setup_cdc.sh
@@ -1,12 +1,12 @@
  #!/bin/bash
 # First enable Logging
-export ORACLE_SID=AI
+export ORACLE_SID=FREE
 echo "enable logging"
 sqlplus /nolog @/opt/oracle/scripts/setup/01_setup_database.sql
 echo "logging enabled"
 # Install user, and data
-sqlplus sys/confluent123@AIPDB1 as sysdba @/opt/oracle/scripts/setup/02_create_user.sql
-sqlplus ordermgmt/kafka@AIPDB1 @/opt/oracle/scripts/setup/03_create_schema_datamodel.sql
-sqlplus ordermgmt/kafka@AIPDB1 @/opt/oracle/scripts/setup/04_load_data.sql
+sqlplus sys/confluent123@FREEPDB1 as sysdba @/opt/oracle/scripts/setup/02_create_user.sql
+sqlplus ordermgmt/kafka@FREEPDB1 @/opt/oracle/scripts/setup/03_create_schema_datamodel.sql
+sqlplus ordermgmt/kafka@FREEPDB1 @/opt/oracle/scripts/setup/04_load_data.sql
 # Create CDC User and align all roles
-sqlplus sys/confluent123@AIPDB1 as sysdba @/opt/oracle/scripts/setup/05_23ai_privs.sql
+sqlplus sys/confluent123@FREEPDB1 as sysdba @/opt/oracle/scripts/setup/05_23ai_privs.sql
